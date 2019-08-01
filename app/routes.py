214bc7880397e24644aebe1885140e41a38b9947
EC2 Default User <ec2-user@ip-172-31-41-97.us-east-2.compute.internal>
2019-08-01 19:03:41 +0000

    Finance

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 app.secret_key= b'\xa0C\xf8\x87\x1b31\xd6\x1fvB\x1b\xee\x07Bq'
 
 # name of database
-app.config['MONGO_DBNAME'] = 'login-name' 
+app.config['MONGO_DBNAME'] = 'login' 
 
 # URI of database
 app.config['MONGO_URI'] = 'mongodb+srv://admin:1234567890@cluster0-tctju.mongodb.net/login?retryWrites=true&w=majority' 
@@ -25,15 +25,15 @@ def index():
 
 # CONNECT TO DB, ADD DATA
 
-@app.route('/add')
+# @app.route('/add')
 
-def add():
-    # connect to the database
+# def add():
+#     # connect to the database
 
-    # insert new data
+#     # insert new data
 
-    # return a message to the user
-    return ""
+#     # return a message to the user
+#     return ""
 
 # sign-up
 @app.route('/signup', methods = ['GET', 'POST'])
@@ -43,8 +43,8 @@ def signup():
         users= mongo.db.users
         existing_user = users.find_one({"username":request.form['username']})
         if existing_user is None:
-            users.insert({"username":request.form['username'], "password":request.form['password'], "first-name":request.form['first-name'], "last-name":request.form['last-name'], 'email':request.form['email']})
-            return "Account created."
+            users.insert({"username":request.form['username'], "password":request.form['password'], "first-name":request.form['first-name'], "last-name":request.form['last-name'], 'email':request.form['email'], 'phone-number':request.form['phone-number']})
+            return render_template('accountconfirmation.html')
         else:
             message = "Username already taken. Try logging in, or try a different username."
             return render_template('signup.html', message= message)
@@ -61,7 +61,7 @@ def login():
         if existing_user: 
           # check if the password is right
           if existing_user['password'] == request.form['password'] :
-              session['username'] = request.form['username']
+              session['username'] = request.form['username'],
               return redirect(url_for('index'))
           else:
               return "Invalid username or password. Try again"
@@ -76,3 +76,27 @@ def login():
 def logout():
     session.clear()
     return redirect('/')
+    
+#Mission Statement:
+@app.route('/missionstatement')
+def missionstatement():
+    return render_template('missionstatement.html')
+    
+#Finaces 
+@app.route('/finances', methods= ["GET", "POST"])
+def finaces():
+    if request.method== "GET":
+        tasks= mongo.db.tasks
+        tasks= tasks.find()
+        return render_template('finances.html', tasks=tasks)
+    
+    # connect to the database
+    else:
+        tasks= mongo.db.tasks
+        # insert new data
+        userdata = dict(request.form)
+        tasks.insert(userdata)
+        print(userdata)
+        tasks= tasks.find()
+        # return a message to the user
+        return render_template('finances.html', tasks=tasks)
